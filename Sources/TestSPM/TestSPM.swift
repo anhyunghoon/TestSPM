6063ee186e5cd45e01d1c4b971e03113cc1681dc
라이나생명 <admin@STARBUCK.local>
2025-04-01 14:17:22 +0900

    SPM test commit

---
+++ Sources/TestSPM/TestSPM.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public class TestSPM {
+    public init() {
+        
+    }
+    
+    public func sayHello(name: String) {
+        print("\(name) Hello")
+    }
+}
